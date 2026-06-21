141682402054301ad2f4553186587e2715317854
p-obrthr <soc_obthr@web.de>
2026-06-21 18:40:24 +0200

    even shorter

--- haskell/17-01/Main.hs
+++ haskell/17-01/Main.hs
@@ -4,7 +4,5 @@ import Text.Printf
 main = do
   xs <- map digitToInt . head . lines <$> readFile "input.txt"
   let solve xs n = sum [x | (x, y) <- zip xs (drop n (cycle xs)), x == y]
-  printf
-    "partOne: %d\npartTwo: %d\n"
-    (solve xs 1)
-    (solve xs (length xs `div` 2))
+  printf "partOne: %d\n" (solve xs 1)
+  printf "partTwo: %d\n" (solve xs (length xs `div` 2))
